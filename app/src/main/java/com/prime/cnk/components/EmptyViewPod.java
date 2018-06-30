c1aa8009c7310b8a404adab7efcab72eb7abd7d0
yepyaesonetun <yepyaesone.1995@gmail.com>
2018-06-30 14:33:47 +0630

    Added loadMoreData with PageIndex and Setup EmptyViewPod

---
+++ app/src/main/java/com/prime/cnk/components/EmptyViewPod.java
@@ -0,0 +1,52 @@
+package com.prime.cnk.components;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.prime.cnk.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by yepyaesonetun on 6/30/18.
+ **/
+
+public class EmptyViewPod extends RelativeLayout {
+
+    @BindView(R.id.iv_empty)
+    ImageView ivEmpty;
+
+    @BindView(R.id.tv_empty)
+    TextView tvEmpty;
+
+    public EmptyViewPod(Context context) {
+        super(context);
+    }
+
+    public EmptyViewPod(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EmptyViewPod(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        ButterKnife.bind(this, this);
+    }
+
+    public void setEmptyData(int emptyImageId, String emptyMsg) {
+        ivEmpty.setImageResource(emptyImageId);
+        tvEmpty.setText(emptyMsg);
+    }
+
+    public void setEmptyData(String emptyMsg) {
+        tvEmpty.setText(emptyMsg);
+    }
+}
